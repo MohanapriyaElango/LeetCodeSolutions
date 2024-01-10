7e192c47efae53b92f1067abb35ca58eb24e7dac
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-11 00:09:59 +0530

    Time: 1 ms (100.00%), Space: 46.3 MB (7.51%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,42 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i=0,j=0;
-        ArrayList<Integer> a= new ArrayList<Integer>();
+        int i=0,j=0,ind=0;
+        int[] a= new int[nums1.length+nums2.length];
         while(i<nums1.length && j<nums2.length)
         {
             if(nums1[i]<nums2[j])
             {
-                a.add(nums1[i]);
+                a[ind++]=nums1[i];
                 i++;
             }
             else if(nums1[i]>nums2[j])
             {
-                a.add(nums2[j]);
+                a[ind++]=nums2[j];
                 j++;
             }
             else if(nums1[i]==nums2[j])
             {
-                a.add(nums1[i]);
-                a.add(nums2[j]);
+                a[ind++]=nums1[i];
+                a[ind++]=nums2[j];
                 i++;
                 j++;
             }
         }
         while(i<nums1.length)
         {
-            a.add(nums1[i]);
+            a[ind++]=nums1[i];
             i++;
         }
         while(j<nums2.length)
         {
-            a.add(nums2[j]);
+            a[ind++]=nums2[j];
             j++;
         }
-        int mid=(a.size()/2);
-        if(a.size()%2==0)
+        int mid=(a.length/2);
+        if(a.length%2==0)
         {
-            return ((double)(a.get(mid)+a.get(mid-1))/2);
+            return ((double)(a[mid]+a[mid-1])/2);
         }
-        return a.get(mid);
-    }
-}
+        return a[mid];
+    }}
