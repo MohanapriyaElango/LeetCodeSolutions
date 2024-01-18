fadba5d258c6414ceb4bea5606b4460589747efb
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-18 18:17:29 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (51.69%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int[] prefix= new int[gain.length+1];
+        prefix[0]=0;
+        int max=prefix[0];
+        for(int i=1;i<=gain.length;i++)
+        {
+            prefix[i]=prefix[i-1]+gain[i-1];
+            if(prefix[i]>=max)
+                max=prefix[i];
+        }
+        return max;
+    }
+}
