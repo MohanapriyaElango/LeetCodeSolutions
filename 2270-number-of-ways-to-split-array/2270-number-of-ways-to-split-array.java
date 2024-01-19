32c627e1f863dc6f61ed57d018f5966543bcd804
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-20 02:42:33 +0530

    Time: 3 ms (65.69%), Space: 61.1 MB (29.12%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -22,29 +22,3 @@ class Solution {
 }
 
 
-// class Solution {
-//     public int waysToSplitArray(int[] nums) {
-//         int ans=0;
-//         long[] left= new long[nums.length+1];
-//         long[] right= new long[nums.length+1];
-//         left[0]= 0;
-//         for(int i=1;i<=nums.length;i++)
-//         {
-//             left[i]=left[i-1]+nums[i-1];
-//         }
-//         // for(int i:left)
-//         //     System.out.println("left:"+i);
-//         right[nums.length]=0;
-//         for(int i=nums.length-1;i>=0;i--)
-//         {
-//             right[i]=right[i+1]+nums[i];
-//         }
-        
-//         for(int i=1;i<left.length-1;i++)
-//         {
-//             if(left[i]>=right[i])
-//                 ans++;
-//         }
-//         return ans;
-//     }
-// }
