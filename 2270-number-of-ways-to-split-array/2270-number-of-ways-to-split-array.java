d4e8c1e0e5e31313c335350e182cd74fe507767a
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-20 02:43:09 +0530

    Time: 4 ms (24.83%), Space: 59.8 MB (47.86%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,24 +1,26 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long left=0,right=0;
         int ans=0;
-        long[] prefix= new long[nums.length];
-        prefix[0]=nums[0];
-        for(int i=1;i<nums.length;i++)
+        long[] left= new long[nums.length+1];
+        long[] right= new long[nums.length+1];
+        left[0]= 0;
+        for(int i=1;i<=nums.length;i++)
         {
-            prefix[i]=prefix[i-1]+nums[i];
+            left[i]=left[i-1]+nums[i-1];
         }
-        for(int i=0;i<nums.length-1;i++)
+        // for(int i:left)
+        //     System.out.println("left:"+i);
+        right[nums.length]=0;
+        for(int i=nums.length-1;i>=0;i--)
         {
-            left=prefix[i];
-            right=prefix[nums.length-1]-prefix[i];
-            if(left>=right)
-            {
+            right[i]=right[i+1]+nums[i];
+        }
+        
+        for(int i=1;i<left.length-1;i++)
+        {
+            if(left[i]>=right[i])
                 ans++;
-            }
         }
         return ans;
     }
 }
-
-
