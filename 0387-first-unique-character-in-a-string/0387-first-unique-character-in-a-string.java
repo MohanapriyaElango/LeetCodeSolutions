61efc0c33b842b50afd1302482036e3611cb012d
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-24 00:13:48 +0530

    Time: 22 ms (54.91%), Space: 45.5 MB (6.56%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,52 +1,11 @@
 class Solution {
     public int firstUniqChar(String s) {
-        // HashMap<Character,Integer> a= new HashMap<Character,Integer>();
-        // char b='$';
-        // int ans=-1;
-        // for(char i: s.toCharArray())
-        // {
-        //     a.put(i,a.getOrDefault(i,0)+1);
-        // }
-        // for(char i: s.toCharArray())
-        // {
-        //     if(a.get(i)==1)
-        //     {
-        //         b=i;
-        //         break;
-        //     }
-        // }
-        // if(b=='$')
-        // {
-        //     return -1;
-        // }
-        // for(int i=0;i<s.length();i++)
-        // {
-        //     if(s.charAt(i)==b)
-        //     {
-        //         ans=i;
-        //         break;
-        //     }
-        // }
-        // return ans;
-        char a='$';
-        int ans=0;
+        int ans=-1;
         for(char i:s.toCharArray())
         {
             if(s.indexOf(i) == s.lastIndexOf(i))
             {
-                a=i;
-                break;
-            }
-        }
-        if(a=='$')
-        {
-            return -1;
-        }
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)==a)
-            {
-                ans=i;
+                ans=s.indexOf(i);
                 break;
             }
         }
