dfa02da098633e62680d20c7cc57a667bf9a88c7
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-23 23:27:38 +0530

    Time: 26 ms (43.13%), Space: 45 MB (31.38%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer> a= new HashMap<Character,Integer>();
+        char b='$';
+        int ans=-1;
+        for(char i: s.toCharArray())
+        {
+            a.put(i,a.getOrDefault(i,0)+1);
+        }
+        for(char i: s.toCharArray())
+        {
+            if(a.get(i)==1)
+            {
+                b=i;
+                break;
+            }
+        }
+        if(b=='$')
+        {
+            return -1;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==b)
+            {
+                ans=i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
