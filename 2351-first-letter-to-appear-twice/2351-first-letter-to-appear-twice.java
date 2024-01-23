c385e5206beec7d41e9bc684e082852375c70761
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-23 22:47:25 +0530

    Time: 1 ms (21.75%), Space: 41.3 MB (40.26%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashMap<Character,Integer> a= new HashMap<Character,Integer>();
+        char ans='a';
+        for(char i: s.toCharArray())
+        {
+            a.put(i,a.getOrDefault(i,0)+1);
+            if(a.get(i)==2)
+            {
+              ans= i;
+              break;
+            }
+        }
+        return ans;
+    }
+}
