967e452047e2ef0ff0c358b2c1adc76e0491774a
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-24 00:10:49 +0530

    Time: 22 ms (54.91%), Space: 45.3 MB (9.78%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,27 +1,50 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character,Integer> a= new HashMap<Character,Integer>();
-        char b='$';
-        int ans=-1;
-        for(char i: s.toCharArray())
+        // HashMap<Character,Integer> a= new HashMap<Character,Integer>();
+        // char b='$';
+        // int ans=-1;
+        // for(char i: s.toCharArray())
+        // {
+        //     a.put(i,a.getOrDefault(i,0)+1);
+        // }
+        // for(char i: s.toCharArray())
+        // {
+        //     if(a.get(i)==1)
+        //     {
+        //         b=i;
+        //         break;
+        //     }
+        // }
+        // if(b=='$')
+        // {
+        //     return -1;
+        // }
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(s.charAt(i)==b)
+        //     {
+        //         ans=i;
+        //         break;
+        //     }
+        // }
+        // return ans;
+        char a='$';
+        int ans=0;
+        for(char i:s.toCharArray())
         {
-            a.put(i,a.getOrDefault(i,0)+1);
-        }
-        for(char i: s.toCharArray())
-        {
-            if(a.get(i)==1)
+            if(s.indexOf(i) == s.lastIndexOf(i))
             {
-                b=i;
+                a=i;
                 break;
             }
         }
-        if(b=='$')
+        if(a=='$')
         {
             return -1;
         }
         for(int i=0;i<s.length();i++)
         {
-            if(s.charAt(i)==b)
+            if(s.charAt(i)==a)
             {
                 ans=i;
                 break;
