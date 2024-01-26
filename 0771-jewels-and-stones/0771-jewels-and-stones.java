a1509691a7241d64c2c4ba3e5dd655be365aba12
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-26 21:40:02 +0530

    Time: 2 ms (17.08%), Space: 41.7 MB (41.81%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        Map<Character,Integer> a= new HashMap<Character,Integer>();
+        for(char i: jewels.toCharArray())
+        {
+            a.put(i,0);
+        }
+        for(char i: stones.toCharArray())
+        {
+            if(a.containsKey(i))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
