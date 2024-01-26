9c7388a9dc6d327ff3e9f86f9c3fd3ff57c1e09a
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-27 00:32:47 +0530

    Time: 13 ms (28.23%), Space: 45.4 MB (35.52%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        StringBuilder a= new StringBuilder(s);
+        int i=0,j=s.length()-1;
+        while(i<j)
+        {
+            if(a.charAt(i)==a.charAt(j))
+            {
+                i++;
+                j--;
+            }
+            else{
+                if((int)a.charAt(i)>(int)(a.charAt(j)))
+                {
+                    a.setCharAt(i,a.charAt(j));
+                }
+                else if((int)a.charAt(i)<(int)(a.charAt(j)))
+                {
+                    a.setCharAt(j,a.charAt(i));
+                }
+                i++;
+                j--;
+            }
+        }
+        String ans= new String(a);
+        return ans;
+    }
+}
