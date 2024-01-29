b47149af9a81ccfd1bd329770d7a070cd60556fc
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-30 00:36:23 +0530

    Time: 9 ms (6.46%), Space: 43.3 MB (5.00%) - LeetHub

---
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] evenOddBit(int n) {
+        String a="";
+        while(n>=1)
+        {
+            int k=n%2;
+            a=k+a;
+            n=n/2;
+        }
+        // System.out.println(a);
+        int even=0;
+        int odd=0;
+        for(int i=0;i<a.length();i++)
+        {
+            if(i%2==0 && a.charAt(a.length()-1-i)=='1')
+            even++;
+            else if(a.charAt(a.length()-1-i)=='1')
+            odd++;
+        }
+        int []ans= new int[2];
+        ans[0]=even;
+        ans[1]=odd;
+        return ans;
+    }
+}
