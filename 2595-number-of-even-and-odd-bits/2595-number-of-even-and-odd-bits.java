98d8ba5173f9f260f312c79c302b2dd1f1fe8cca
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-30 00:42:12 +0530

    Time: 2 ms (41.46%), Space: 42.3 MB (39.38%) - LeetHub

--- 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.java
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] evenOddBit(int n) {
-        String a="";
-        while(n>=1)
-        {
-            int k=n%2;
-            a=k+a;
-            n=n/2;
-        }
+        //String a="";
+        // this is will used to convert decimal to binary
+        // while(n>=1)
+        // {
+        //     int k=n%2;
+        //     a=k+a;
+        //     n=n/2;
+        // }
         // System.out.println(a);
+        String a=Integer.toBinaryString(n);
         int even=0;
         int odd=0;
         for(int i=0;i<a.length();i++)
