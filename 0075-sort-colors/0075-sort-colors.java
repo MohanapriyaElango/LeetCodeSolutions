ddb2cb96d8fcd4ee82e022ad797679dead7c5bf1
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-01-30 20:03:18 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (57.30%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,44 +1,43 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int i=0,j=1;
-        while(i<nums.length-1)
-        {
-            if(nums[i]>nums[j])
-            {
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-            }
-            if(j==nums.length-1)
-            {
-                i++;
-                j=i+1;
-            }
-            else
-            j++;}
+        // int i=0,j=1;
+        // while(i<nums.length-1)
+        // {
+        //     if(nums[i]>nums[j])
+        //     {
+        //         int temp=nums[i];
+        //         nums[i]=nums[j];
+        //         nums[j]=temp;
+        //     }
+        //     if(j==nums.length-1)
+        //     {
+        //         i++;
+        //         j=i+1;
+        //     }
+        //     else
+        //     j++;}
 
         //THREE POINTERS AND ONE PASS- ALGO
-        // int left=0,right=nums.length-1,i=0;
-        // while(left<right)
-        // {
-        //     if(nums[i]==0)
-        //     {
-        //         int temp=nums[i];
-        //         nums[i]=nums[left];
-        //         nums[left]=temp;
-        //         left++;
-        //     }
-        //     else if(nums[i]==2)
-        //     {
-        //         int temp=nums[i];
-        //         nums[i]=nums[right];
-        //         nums[right]=temp;
-        //         right--;
-        //     }
-        //     i++;
-        //     for(int k:nums)
-        //     System.out.print(k);
-        //     System.out.println();
-        // }
+        int left=0,right=nums.length-1,i=0;
+        while(i<nums.length && i<=right)
+        {
+            if(nums[i]==0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[left];
+                nums[left]=temp;
+                left++;
+                i++;
+            }
+            else if(nums[i]==2)
+            {
+                int temp=nums[i];
+                nums[i]=nums[right];
+                nums[right]=temp;
+                right--;
+            }
+            else
+            i++;
+        }
     }
 }
