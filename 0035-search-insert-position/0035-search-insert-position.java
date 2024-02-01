44b725a9ec4bd67a54f95bc71e6c5ff54432e723
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-01 18:32:33 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (88.53%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i=0,j=nums.length-1,mid=0;
+        while(i<=j)
+        {
+            mid=(i+j)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            j=mid-1;
+            else if(nums[mid]<target)
+            i=mid+1;
+        }
+        return i;
+    }
+}
