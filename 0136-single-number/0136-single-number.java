6cbeeb83efcd2f6f738824e4f46fe3ef54f059cc
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-05 00:20:32 +0530

    Time: 1 ms (99.88%), Space: 46.2 MB (32.33%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int ans=0;
-        for(int i:nums)
-        ans^=i;
+        for(int i:nums)   ans^=i;
         return ans;
     }
 }
