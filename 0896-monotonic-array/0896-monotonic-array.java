ed6ecfe58e63f6be6ab42f086b82b044464ca53c
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-06 18:56:11 +0530

    Time: 47 ms (5.94%), Space: 56.9 MB (80.66%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,65 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        Stack<Integer> a= new Stack<Integer>();
+        a.push(nums[0]);
+        if(nums.length==1)
+        return true;
+        // else if(nums.length==2 && (nums[0]<=nums[1] || nums[0]>=nums[1]))
+        // {
+        //     return true;
+        // }
+        // if((nums[0]<=nums[1] && nums[0]<=nums[2]) && nums[0]<=nums[nums.length-1] )
+        // {
+        //     for(int i=1;i<nums.length;i++){
+        //         // System.out.println(a);
+        //     if(!a.empty() && a.peek()<=nums[i])
+        //     {
+        //         a.pop();
+        //     }
+        //     a.push(nums[i]);
+        //    }
+        //    if(a.size()==1)
+        //    return true;
+        // }
+        // else
+        // {
+        //     for(int i=1;i<nums.length;i++){
+        //     if(!a.empty() && a.peek()>=nums[i])
+        //     {
+        //         a.pop();
+        //     }
+        //     a.push(nums[i]);
+        //    }
+        //    if(a.size()==1)
+        //    return true;
+        // }
+        // return false;
+
+        if(nums[0]<=nums[nums.length-1] )
+        {
+            for(int i=1;i<nums.length;i++){
+                // System.out.println(a);
+            if(!a.empty() && a.peek()<=nums[i])
+            {
+                a.pop();
+            }
+            a.push(nums[i]);
+           }
+           if(a.size()==1)
+           return true;
+        }
+        else
+        {
+            for(int i=1;i<nums.length;i++){
+            if(!a.empty() && a.peek()>=nums[i])
+            {
+                a.pop();
+            }
+            a.push(nums[i]);
+           }
+           if(a.size()==1)
+           return true;
+        }
+        return false;
+    }
+}
