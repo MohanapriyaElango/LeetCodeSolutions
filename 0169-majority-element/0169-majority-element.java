2db03108c82ebbcd76c618ff5abc24f908da570b
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-08 22:58:04 +0530

    Time: 18 ms (13.33%), Space: 48.4 MB (91.97%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> a= new HashMap<Integer,Integer>();
+        int ans=Integer.MIN_VALUE,count=-1;
+        for(int i: nums)
+        {
+           a.put(i,a.getOrDefault(i,0)+1);
+           if(a.get(i)>=count)
+           {
+               count=a.get(i);
+               ans=i;
+           }
+        }
+        return ans;
+    }
+}
