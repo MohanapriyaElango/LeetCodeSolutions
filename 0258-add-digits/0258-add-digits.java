3ddb5a0fb31aa78304a87be10d0e5e6c29659a1b
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-08 23:51:44 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (10.61%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,16 +1,23 @@
 class Solution {
+    public int sum(int num)
+    {
+        if(num==0)
+            return 0;
+        return (num%10)+sum(num/10);
+    }
     public int addDigits(int num) {
         int sum=0;
         while(num>=10)
         {
-            sum=0;
-            while(num>0)
-            {
-             int r=num%10;
-             sum=sum+r;
-             num=num/10;
-            }
-            num=sum;
+            // sum=0;
+            // while(num>0)
+            // {
+            //  int r=num%10;
+            //  sum=sum+r;
+            //  num=num/10;
+            // }
+            // num=sum;
+            num=sum(num);
         }
         return num;
     }
