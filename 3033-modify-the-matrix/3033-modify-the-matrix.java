d35312e6b15364616eb4931288075927edd868ef
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-11 21:22:36 +0530

    Time: 1 ms (100.00%), Space: 45.7 MB (50.00%) - LeetHub

--- 3033-modify-the-matrix/3033-modify-the-matrix.java
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -1,24 +1,20 @@
 class Solution {
     public int[][] modifiedMatrix(int[][] matrix) {
-        ArrayList<Integer> row= new ArrayList<Integer>();
-        ArrayList<Integer> col= new ArrayList<Integer>();
-        int[] max= new int[matrix[0].length];
+        int max=-1;
         for(int i=0;i<matrix.length;i++)
         {
             for(int j=0;j<matrix[0].length;j++)
             {
                 if(matrix[i][j]==-1)
                 {
-                    row.add(i);
-                    col.add(j);
+                    max=-1;
+                    for(int k=0;k<matrix.length;k++)
+                    {
+                        max=Math.max(max,matrix[k][j]);
+                    }
+                    matrix[i][j]=max;
                 }
-                if(max[j]<=matrix[i][j])
-                    max[j]=matrix[i][j];
             }
-        }
-        for(int i=0;i<row.size();i++)
-        {
-            matrix[row.get(i)][col.get(i)]=max[col.get(i)];
         }
         return matrix;
     }
