23636e553940831e18ce2da069c089499f037486
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-13 01:08:31 +0530

    Time: 1 ms (65.36%), Space: 44.7 MB (75.15%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+       // int [] ans= new int[nums.length];
+        int i=0,j=0;
+        if(nums.length<2)
+        return nums;
+        while(i<=j && i<nums.length && j<nums.length)
+        {
+            if(nums[i]%2==1)
+            {
+                while(j<nums.length && nums[j]%2==1)
+                    j++;
+                if(j<nums.length)
+                {
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                    i++;
+                    j=j+1;
+                }
+            }
+            else{
+                i++;
+                j++;
+            }
+        }
+        return nums;
+    }
+}
