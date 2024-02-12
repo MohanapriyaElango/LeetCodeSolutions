90d72b6ac3d3fec9d2f0cd1629ca02e4d1ca2d5e
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-12 19:41:17 +0530

    Time: 1 ms (41.17%), Space: 40.3 MB (89.53%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        // System.out.println((float)(Math.log10(n)/Math.log10(4)));
+        // return (Math.log10(n)/Math.log10(4))%1==0;
+        if(n==0) return false;
+        if(n==1) return true;
+        if(n%4!=0) return false;
+        return isPowerOfFour(n/4);
+
+    }
+}
