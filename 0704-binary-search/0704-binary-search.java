fa236b4c556ecdf7b76f86fd94cba07db101eb6a
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-02-27 17:44:59 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (50.90%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0,r=nums.length-1,mid;
+        while(l<=r)
+        {
+            mid=(l+r)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                l=mid+1;
+            else if(nums[mid]>target)
+                r=mid-1;
+        }
+        return -1;
+    }
+}
