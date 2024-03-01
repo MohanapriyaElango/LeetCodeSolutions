cfa13fdf829d3a6d10dd8acf580362c96cb85409
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-03-01 23:58:47 +0530

    Time: 2 ms (83.47%), Space: 42.8 MB (98.64%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        int one=0,zero=0;
+        for(char i: s.toCharArray())
+        {
+            if(i=='0') zero++;
+            else one++;
+        }
+        String ans="";
+        ans=ans+("1".repeat(one-1));
+        ans=ans+("0".repeat(zero));
+        ans=ans+"1";
+        return ans;
+    }
+}
