11f21466697f5a6e981406a3693ccbc0a131111a
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-06-27 22:34:22 +0530

    Time: 5 ms (91.37%), Space: 44 MB (96.09%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,16 +1,32 @@
 class Solution {
     public int minMoves2(int[] nums) {
         
-        //MEDIAN METHOD
+//         //MEDIAN METHOD
         
-        int n= nums.length;
+//         int n= nums.length;
+//         Arrays.sort(nums);
+//         int median = n%2==0 ? (nums[n/2]+nums[(n/2)-1])/2 : nums[n/2];
+        
+//         int ans = 0;
+//         for(int i: nums)
+//         {
+//             ans+=Math.abs(i-median);
+//         }
+//         return ans;
+        
+        
+        
+        //TWO - POINTERS METHOD
+        
+        int left =0;
+        int right = nums.length-1;
         Arrays.sort(nums);
-        int median = n%2==0 ? (nums[n/2]+nums[(n/2)-1])/2 : nums[n/2];
-        
-        int ans = 0;
-        for(int i: nums)
+        int ans=0;
+        while(left<=right)
         {
-            ans+=Math.abs(i-median);
+            ans+=Math.abs(nums[left]-nums[right]);
+            left++;
+            right--;
         }
         return ans;
     }
