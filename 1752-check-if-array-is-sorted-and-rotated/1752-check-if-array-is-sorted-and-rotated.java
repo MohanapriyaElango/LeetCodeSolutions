2b9fc837d2b23aea5160c1a86b13e51896f4c24f
Mohanapriya Elango <142934253+MohanapriyaElango@users.noreply.github.com>
2024-06-30 20:22:22 +0530

    Time: 0 ms (100.00%), Space: 41 MB (66.66%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,54 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int min = nums[0];
+        int minPosition = 0;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i-1]>nums[i])
+            {
+                minPosition = i;
+                break;
+            }  
+        }
+        int i= 0;
+        int j= minPosition-1;
+        while(i<j)
+        {
+            nums[i] = nums[i]^nums[j];
+            nums[j] = nums[i]^nums[j];
+            nums[i] = nums[i]^nums[j];
+            i++;
+            j--;
+        }
+        i= minPosition;
+        j= nums.length-1;
+        while(i<j)
+        {
+            nums[i] = nums[i]^nums[j];
+            nums[j] = nums[i]^nums[j];
+            nums[i] = nums[i]^nums[j];
+            i++;
+            j--;
+        }
+        i=0;
+        j=nums.length-1;
+        while(i<j)
+        {
+            nums[i] = nums[i]^nums[j];
+            nums[j] = nums[i]^nums[j];
+            nums[i] = nums[i]^nums[j];
+            i++;
+            j--;
+        }
+        boolean sorted = true;
+        for(i=1;i<nums.length;i++)
+        {
+            if(!(nums[i-1]<=nums[i]))
+            {
+                sorted=false;
+                break;
+            }
+        }
+        return sorted;
+    }
+}
